5fe96e5b485e20255cfbc247c13907dcc6d009bc
yusufalp <alpyusuf007@gmail.com>
2023-11-29 23:13:26 -0800

    add content, update href links

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,14 +26,26 @@ const Home = () => {
 
   return (
     <React.Fragment>
-      <h1>Books</h1>
+      <h1>CodeSquad Comics</h1>
+      <p class="index-top-p">
+        CodeSquad Comics is a collection of graphic novels read by Aya Yamamoto.
+        The site is intended to display comic book covers along with information
+        about each book, including the author, a rating, and other details about
+        the graphic novel. Browse through the complete collection below. Click
+        on the cover image or the Details link to see even more information for
+        each graphic novel including the publisher, genre, number of pages, and
+        a brief synopsis. The About page includes meta information about this
+        collection. Login is only available to the site administrator at this
+        time.
+      </p>
+      <h2>Complete Collection</h2>
       <div className="book-container">
         {isLoading ? (
           <p>Loading...</p>
         ) : (
           books.map((book) => (
             <div key={book._id} className="container-single-book-index">
-              <a href="#">
+              <a href={`/books/${book._id}`}>
                 <img
                   className="image-cover-index"
                   src={`/images/${book.image}`}
@@ -43,7 +55,7 @@ const Home = () => {
               <br />
               <p>{book.title}</p>
               <p>{book.rating} stars</p>
-              <a href="#">Details</a>
+              <a href={`/books/${book._id}`}>Details</a>
             </div>
           ))
         )}
